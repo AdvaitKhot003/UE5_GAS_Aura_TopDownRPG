008f92b26125da7d23334bff75a805020fa7322e
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-02-02 19:00:16 +0530

    Aura Player Controller

--- Source/Aura/Private/Controller/AuraPlayerController.cpp
+++ Source/Aura/Private/Controller/AuraPlayerController.cpp
@@ -2,4 +2,31 @@
 
 
 #include "Controller/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInput/DataAsset_AuraInputConfig.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(AuraInputConfig);
+	
+	if (!IsLocalController()) return;
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (!Subsystem) return;
+	Subsystem->AddMappingContext(AuraInputConfig->InputMappingContext, 0);
+	
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
--- Source/Aura/Public/Controller/AuraPlayerController.h
+++ Source/Aura/Public/Controller/AuraPlayerController.h
@@ -6,6 +6,8 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UDataAsset_AuraInputConfig;
+
 /**
  * 
  */
@@ -14,4 +16,12 @@ class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AAuraPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input|DataAsset")
+	TObjectPtr<UDataAsset_AuraInputConfig> AuraInputConfig;
 };
