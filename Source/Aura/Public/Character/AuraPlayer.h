cdffd99d4f5fb75b58d4dce9c0169d4cfb186b9d
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-02-02 13:55:40 +0530

    Player and Enemy Characters

---
+++ Source/Aura/Public/Character/AuraPlayer.h
@@ -0,0 +1,17 @@
+// No Copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayer : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
