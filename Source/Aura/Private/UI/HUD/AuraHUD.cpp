8f0532f85ec4eacc7d4ae8ecc4af3d2a6caa0a9f
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-02-05 22:53:22 +0530

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,16 @@
+// No Copyright
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	OverlayWidget = CreateWidget<UAuraUserWidget>(GetWorld(), OverlayWidgetClass);
+	if (OverlayWidget)
+	{
+		OverlayWidget->AddToViewport();
+	}
+}
