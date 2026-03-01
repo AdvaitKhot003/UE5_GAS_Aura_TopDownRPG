693648dfaf0ed4f2f328811dc10c857391fc0583
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-03-01 21:17:12 +0530

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/Controller/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// No Copyright
+
+
+#include "UI/Controller/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
